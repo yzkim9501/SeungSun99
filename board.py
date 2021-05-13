9f5576af0a14d05578d479f3a821ced06f585dee
jooyeon-han <65266381+jooyeon-han@users.noreply.github.com>
2021-05-13 18:18:55 +0900

    Update_board

--- board.py
+++ board.py
@@ -35,57 +35,72 @@ def read_board():
 
 
 ## (Create & Update) API 역할을 하는 부분  (이 부분 조언 구하기)
+## title 미작성시 글이 작성되는 것을 막아야함.
 @bp.route('/api/create_board', methods=['POST'])
 @login_required
 def create_board():
     print(request.form)
     if request.method == "POST":
         title = request.form["post-name"]
-        question_content = request.form["post-content"]  ## 구글링으로 일단 넣어봄
+        question_content = request.form["post-content"]
 
         # (Create를 할때!)게시물 인덱싱 조건문 추가
-        if db.board.count_documents({}) == 0:  # board 테이블에 document 가 없으면
+        if db.board.count_documents({}) == 0:
             index = 1
         else:  # document 가 있으면
-            data = list(db.board.find({}).sort('_id', pymongo.DESCENDING).limit(1))  ## 이제_id를 정렬기준으로 사용함. 가장 최근 생성된 도큐먼트 1개 선택
-            data1 = data[0]  ## 인덱스를 담고
-            index = data1['_id'] + 1 ##인데스+1
+            data = list(db.board.find({}).sort('_id', pymongo.DESCENDING).limit(1))
+            data1 = data[0]
+            index = data1['_id'] + 1
 
 
         doc = ({
-                   '_id': index,  # 게시판 인덱스는 숫자로
+                   '_id': index,
                    'post-name': title,
                    'post-content': question_content,
-                   'date': time.strftime('%y-%m-%d %H:%M:%S'), ##해결!
+                   'date': time.strftime('%y-%m-%d %H:%M:%S'),
                    'user_id': session['user_id'],
-                   'user_name': session['user_name'],  ## How? 유저아이디인지 유저이름인지 확인필요 따로따로 저장하고 표시는 이름으로
+                   'user_name': session['user_name'],
         })
-        db.board.update_one({}, {'$set': {'index': +1}})
-        ##업데이트시, 1)정보를 불러와야함 2)새로 입력한 값으로 바꿈.
+
+
         db.board.insert_one(doc)
 
         return redirect(request.referrer)
 
-## if post-tilte == '빈 문자열' return ()
-## (Delete) index를 받아서 get 방식으로 게시물 글 지우기!
+
+@bp.route('/api/update_board', methods=['POST'])
+@login_required
+def update_board():
+    print(request.form)
+    if request.method == "POST":
+        id_receive = request.form.get('board-id')
+        data = db.board.find_one({'_id': id_receive})
+
+        if session.get('user_id') == data['user_id']:
+            new_title = request.form['post-name'],
+            new_content = request.form['post-content']
+
+            db.board.update_one({'_id': int(id_receive)},
+                                {'$SET': {'post-name': new_title,
+                                          'post-content': new_content}})
+            return redirect(request.referrer)
+        else:
+            return jsonify({'msg': '권한이 없습니다.'})
+
 
 @bp.route('/api/delete_board', methods=['GET'])
 @login_required
 def delete_board():
     delete1 = request.args.get('index')
-    db.board.delete_one({'_id': int(delete1)})
-    return jsonify({'msg': '삭제되었습니다'})
-    ##삭제시 비밀번호 필요한가?
-    ##remove???
-    ##게시글 수정도 하나..?
+    db.board.delete_one({'index': int(delete1)})
+
+    return jsonify({'msg': '게시물이 삭제되었습니다.'})
 
 
-##(find 사용) [게시판 목록을 조회할때 볼 수 있는 화면]을 부르는 기능
-##인덱스, 제목, 작성자, 작성일 중 index를 기준으로 작성
-##게시판의 해당 글(특정 글)을 불러오는 것 ! (study.py의 study_target참조)
 @bp.route('/api/board_target', methods=['GET'])
 @login_required
 def board_target():
     target = request.args.get('index')
     board_target = db.board.find_one({'_id': int(target)}) ##int와 같이 알고 있는 부분도 구현할 수 있도록 해보자
     return jsonify(board_target)
+
